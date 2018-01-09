ec33664a8c9db571597966cb21852f25cd812398
Stefyly <Stepah3@gmail.com>
2018-01-09 11:55:19 +0200

    first autotest

---
+++ autotest.rb
@@ -0,0 +1,57 @@
+require "Watir"
+require "RSpec"
+require_relative "func.rb"
+include ::RSpec::Matchers
+
+#Brows.new.ss(1)
+# bmw = Car.new
+
+# bmw.set("ff")
+
+# p bmw.get
+
+# b = Watir::Browser.new :chrome
+# goto(b, "weblium.com")
+# expect(b.url).to eql("https://weblium.com/")
+# goto_login_page(b)
+# expect(get_title(b)).to eql("Log in")
+# login(b, "weblium1@gmail.com", 123124)
+# expect(get_login_error_text(b)).to eql("Invalid email or password")
+# set_pass(b, 123123)
+# click_login_button(b)
+# hover_goto_site_settings(b, 1)
+# expect(get_title(b)).to eql("Manage general settings")
+# site_name = get_site_name_from_banner(b)
+# set_save_site_name(b, site_name + "_test")
+# expect(get_site_name_from_banner(b)).to eql(site_name + "_test")
+# goto_my_websites(b)
+# expect(check_site_name(b)).to eql(site_name + "_test")
+# hover_goto_site_settings(b, 1)
+# set_save_site_name(b, site_name)
+# expect(get_site_name_from_banner(b)).to eql(site_name)
+# b.close
+b = Watir::Browser.new :chrome
+page = Browse.new(b)
+page.goto('https://weblium.com/')
+    .goto_login_page
+sleep 10
+# b1.set(b)
+# goto("weblium.com")
+# expect(b.url).to eql("https://weblium.com/")
+# goto_login_page()
+# expect(get_title()).to eql("Log in")
+# login("weblium1@gmail.com", 123124)
+# expect(get_login_error_text()).to eql("Invalid email or password")
+# set_pass(123123)
+# click_login_button()
+# hover_goto_site_settings(1)
+# expect(get_title()).to eql("Manage general settings")
+# site_name = get_site_name_from_banner()
+# set_save_site_name(site_name + "_test")
+# expect(get_site_name_from_banner()).to eql(site_name + "_test")
+# goto_my_websites()
+# expect(check_site_name()).to eql(site_name + "_test")
+# hover_goto_site_settings(1)
+# set_save_site_name(site_name)
+# expect(get_site_name_from_banner()).to eql(site_name)
+# b.close
